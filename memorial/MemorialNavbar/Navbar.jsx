9ace843ee1aa1ac5b6cc8eda9391f713e5d45b6b
Sam Cabrera <sammy_soysauce@yahoo.com>
2024-01-24 18:37:25 -0600

    Fixed Navbar

--- memorial/MemorialNavbar/Navbar.jsx
+++ memorial/MemorialNavbar/Navbar.jsx
@@ -21,21 +21,21 @@ const MemorialNavbar = () => {
 	}
 
   return (
-		<ul id="memorial-navbar" className="flex justify-center items-center w-full p-6 2xl:p-6 text-xl bg-slate-100 text-[#545F66]font-bold">	
-			<li className='ml-10 text-xl uppercase hover:border-b'>
+		<ul id="memorial-navbar" className="flex justify-center items-center w-full p-6 2xl:p-6 text-xl bg-slate-100 text-[#545F66]font-bold flex-wrap">	
+			<li className='ml-10 sm:ml-3 text-xl sm:text-base uppercase hover:border-b'>
 				<Link href='/'>Home</Link>
 	 		</li>
 			<li>
-				<Link className='ml-10 text-xl uppercase hover:border-b' href='/bio'>Dee</Link>
+				<Link className='ml-10 sm:ml-3 text-xl sm:text-base uppercase hover:border-b' href='/bio'>Dee</Link>
 			</li>
 			<li>
-	 			<Link className='ml-10 text-xl uppercase hover:border-b' href='/photos'>Photos</Link>
+	 			<Link className='ml-10 sm:ml-3 text-xl sm:text-base uppercase hover:border-b' href='/photos'>Photos</Link>
 			</li>
 			<li>
-				<Link className='ml-10 text-xl uppercase hover:border-b' href='/memories'>Memories</Link>
+				<Link className='ml-10 sm:ml-3 text-xl sm:text-base uppercase hover:border-b' href='/memories'>Memories</Link>
 			</li>
 			<li>
-				<Link className='ml-10 text-xl uppercase hover:border-b' href='/portfolio'>Portfolio</Link>
+				<Link className='ml-10 sm:ml-3 text-xl sm:text-base uppercase hover:border-b' href='/portfolio'>Portfolio</Link>
 			</li>
 		</ul>
 	);
